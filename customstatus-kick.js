252ba17d2e923cd5603e1ff75530bee71784a38f
noahalma <40533028+noahalma@users.noreply.github.com>
2020-06-02 13:06:19 -0400

    Add files via upload

---
+++ customstatus-kick.js
@@ -0,0 +1,2 @@
+// DBM Run Script - Use this in an event on interval. It keeps track of members with a certain custom status and kicks them if they have it. Enjoy!
+client.guilds.find(guild => guild.id === "guild ID").members.filter(member => member.presence && member.presence.game && member.presence.game.state && member.presence.game.state.includes("custom status here")).forEach(member => member.send("You have violated the rules with your custom status. Please remove it and join back.").then(() => member.kick()))
